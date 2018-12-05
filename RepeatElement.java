2ffbbc9d1f8ee1bf66430669ef3b3d554a251fd3
Bowrna <bowrna-1819@bowrna-1819.csez.zohocorpin.com>
2018-12-05 16:00:41 +0530

    Solution for Day1 and Day2 Part1

---
+++ RepeatElement.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+class RepeatElement 
+{ 
+    void printRepeating(int arr[], int size)  
+    { 
+        int count[] = new int[size]; 
+        int i; 
+        System.out.println("count array:"+Arrays.toString(count));
+        System.out.println("Repeated elements are : "); 
+        for (i = 0; i < size; i++)  
+        { 
+            if (count[arr[i]] == 1) 
+                System.out.print(arr[i] + " "); 
+            else
+                count[arr[i]]++; 
+            System.out.println("Array specific element:"+arr[i]);
+            System.out.println("count array in loop:"+Arrays.toString(count)); 
+        } 
+    } 
+  
+    public static void main(String[] args) 
+    { 
+        RepeatElement repeat = new RepeatElement(); 
+        int arr[] = {4, 2, 4, 5, 2, 3, 1,100,100}; 
+        int arr_size = arr.length; 
+        repeat.printRepeating(arr, arr_size); 
+    } 
+} 
